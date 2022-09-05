773f7ea3b1b7adcce46cf677d9009af57579fd1b
Magnus Ahlberg <magnus.ahlberg@swegon.com>
2022-09-05 10:24:09 +0200

    Added nbgv and example pages

---
+++ TabBlazor.FluentValidation.Server/Pages/TableInline.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using TabBlazor.Components.Modals;
+using TabBlazor.FluentValidation.Server.Validation;
+using TabBlazor.Services;
+
+namespace TabBlazor.FluentValidation.Server.Pages;
+
+public partial class TableInline
+{
+    [Inject] public IModalService Modal { get; set; }
+
+    private IList<Person> Persons { get; set; } = new List<Person>();
+
+    protected override void OnInitialized()
+    {
+        Persons = new List<Person>()
+        {
+            new() { Name = "Magnus", Age = 36 }
+        };
+    }
+
+    private async Task Submit(Person person)
+    {
+        await Modal.ShowDialogAsync(new DialogOptions()
+        {
+            MainText = "Form Is Valid",
+            SubText = $"Person Name: {person.Name}"
+        });
+    }
+}
